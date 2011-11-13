4104a0051c54c8e05b435b9ed702a3b8426b115b
rplante <rplante@git.lsstcorp.org>
2011-11-13 07:29:01 +0000

    developing validateRelease

--- bin/validateRelease.sh
+++ bin/validateRelease.sh
@@ -3,6 +3,8 @@
 SHELL=/bin/bash
 stackbase=/lsst/DC3/stacks/rc
 
+[ -n "$DEVENV_SERVERTOOLS_DIR" -a -e "$DEVENV_SERVERTOOLS_DIR/conf/validateRelease_conf.sh" ] && . $DEVENV_SERVERTOOLS_DIR/conf/validateRelease_conf.sh"
+
 function usage() {
     echo Usage: `basename $0` "[-b DIR -r DIR -h]" product version
 }
@@ -30,6 +32,7 @@ done
 [ -z "$refstack" ]    && refstack=$stackbase/ref
 [ -z "$teststack" ]   && teststack=$stackbase/test
 [ -z "$serverstage" ] && serverstage=$stackbase/pkgs
+[ -z "$builddir" ]    && builddir=$stackbase/build
 [ -d "$refstack" ] || {
     echo "Reference stack directory not found: $refstack"
     exit 2
@@ -42,6 +45,10 @@ done
     echo "Product staging directory not found: $serverstage"
     exit 2
 }
+[ -d "$builddir" ] || {
+    echo "Build directory not found: $builddir"
+    exit 2
+}
 
 function clearlsst {
     setuppkgs=(`printenv | grep '^SETUP_' | sed -e 's/=.*$//'`)
@@ -51,9 +58,47 @@ function clearlsst {
         eval $pkghome=
     done
     EUPS_PATH=
+    LD_LIBRARY_PATH=
+    PYTHONPATH=
+}
+
+function taggedVersion {
+    echo $1 | sed -e 's/[\+\-].*$//'
+}
+
+function prodductDir {
+    echo "$builddir/$1-$2
+}
+
+function reposExtract {
+    cd $builddir
+    
 }
 
 clearlsst
 export LSST_DEVEL=$teststack
 export LSST_HOME=$refstack
+source $LSST_HOME/loadLSST.sh
+
+prodname=$1 ; shift
+version=$1 ; shift
+command=$1 ; shift
+taggedas=`taggedVersion $version`
+pdir=`productDir $prodname $taggedas`
+
+function do_extract {
+    if [ -d "$pdir" ]; then
+        echo "Product is already checked out"
+    else
+        reposExtract
+    fi
+}
+
+
+case $command in
+    extract)
+      do_extract ;;
+    *)
+      echo "Unknown command: $command" ;;
+esac
 
