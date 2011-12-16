ca9b3759894f1a07b7597ac56ce562d98a12fe4a
Ray Plante <rplante@ncsa.uiuc.edu>
2011-12-16 02:25:08 -0600

    tagRelease: support non-lsst products

--- bin/tagRelease.py
+++ bin/tagRelease.py
@@ -13,6 +13,9 @@ lsstMarkerRe = re.compile(r"^\s*#\s*lsst", re.IGNORECASE)
 def options():
     usage="%prog -d DIR product version tag [ tag ... ]"
     parser = optparse.OptionParser(usage=usage)
+    parser.add_option("-t", "--type", dest="section", action="store",
+                      default="lsst", metavar="TYPE",
+                      help="type of product; either external, pseudo, or lsst")
     parser.add_option("-d", "--server-directory", dest="sdir", action="store",
                       metavar="DIR", help="the root directory for the server")
     parser.add_option("-D", "--output-directory", dest="outdir",
@@ -35,7 +38,7 @@ def options():
 
     return (opts, args)
 
-class AmongLSSTEntries(object):
+class AreWeThereYet(object):
     def __init__(self):        self.v =  1
     def no(self):       return self.v != 0
     def yes(self):      return self.v == 0
@@ -65,7 +68,12 @@ def main():
     if not outdir:
         outdir = tmpdir
 
+    nextsectre=re.compile(r"^\s*#\s*\[")
+    sectre=re.compile(r"^\s*#\s*\[%s\]" % opts.section)
     prodre=re.compile(r"^\s*%s\s" % prodname)
+    extradir = ''
+    if opts.section != 'lsst': extradir = opts.section
+
     failures=[]
     for tag in args:
         tagfile = os.path.join(opts.sdir, "%s.list" % tag)
@@ -83,10 +91,15 @@ def main():
             print "Updating %s" % tagfile
 
         fmt = None
+        savedlines = []
         with open(tagfile) as tf:
             with open(ntagfile, 'w') as ntf:
-                amonglsstentries = AmongLSSTEntries()
+                inrightsection = AreWeThereYet()
                 for line in tf:
+                    if line.strip() == '#':
+                        savedlines.append(line)
+                        continue
+
                     line = line.strip("\n")
                     if line.startswith("#---"):
                         cols = line.strip().split()
@@ -95,23 +108,48 @@ def main():
                             widths.append(10)
                         fmt = "%%-%ds  %%-%ds  %%-%ds  %%-%ds  %%-%ds" % \
                               tuple(widths)
+
                     mat = prodre.match(line)
-                    if amonglsstentries.notyet() and \
-                       (line.startswith('afw') or lsstMarkerRe.match(line)):
-                        amonglsstentries.setYes()
                     if mat:
+                        # We've found the record for our product.
+                        # Regardless of which section we're in, update record.
                         cols = line.strip().split()
                         while len(cols) < 5:
                             cols.append('')
                         cols[2] = version
                         line = fmt % tuple(cols)
-                        amonglsstentries.setDone()
-                    elif amonglsstentries.yes() and not line.startswith('#') \
-                         and cmp(prodname, line) < 0:
-                        cols = [prodname, "generic", version, '', '']
-                        print >> ntf, fmt % tuple(cols)
-                        amonglsstentries.setDone()
+                        inrightsection.setDone()
+                    elif inrightsection.notyet():
+                        if sectre.match(line):
+                            # this is the section it belongs in
+                            inrightsection.setYes()
+                    elif inrightsection.yes():
+                        if nextsectre.match(line):
+                            # end of our section; add record
+                            cols = [prodname, "generic", version, extradir, '']
+                            print >> ntf, fmt % tuple(cols)
+                            inrightsection.setDone()
+                        elif not line.startswith('#') and \
+                             cmp(prodname, line) < 0:
+                            # this is the right place alphabetically
+                            if savedlines:
+                                for saved in savedlines:
+                                    ntf.write(saved)
+                                savedlines = []
+                            cols = [prodname, "generic", version, extradir, '']
+                            print >> ntf, fmt % tuple(cols)
+                            inrightsection.setDone()
+
+                    if savedlines:
+                        for saved in savedlines:
+                            ntf.write(saved)
+                        savedlines = []
                     print >> ntf, line
+
+                if not inrightsection.done():
+                    # last chance to add it if we haven't already
+                    cols = [prodname, "generic", version, extradir, '']
+                    print >> ntf, fmt % tuple(cols)
 
         if not opts.outdir:
             try:
