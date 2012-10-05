bb002c7eeb7853a7a31378b9ec65ae8ceee829eb
Jim Bosch <jbosch@astro.princeton.edu>
2012-10-05 13:19:20 -0400

    remove synchronization of current and stable tags

--- lib/rsyncToStdWebServer.sh
+++ lib/rsyncToStdWebServer.sh
@@ -13,11 +13,6 @@ function rsyncToStdWebServer {
     echo rsync -avz $delete --exclude=.git\* --exclude=/newinstall.sh --exclude=\*~ $localServerMirror$subdir/ $testPackageServerDir$subdir
     rsync -avz $delete --exclude=.git\* --exclude=/newinstall.sh --exclude=\*~ $localServerMirror$subdir/ $testPackageServerDir$subdir || return 1
 
-    [ -z "$subdir" ] && {
-        echo "cd $testPackageServerDir && sed -e \'/EUPS distribution/ s/current/stable/\' current.list \> stable.list"
-        cd $testPackageServerDir && sed -e '/EUPS distribution/ s/current/stable/' current.list > stable.list || return 2
-    }
-
     return 0
 }
 
